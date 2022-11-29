8a79dda602627f923f2cec8e2da5b8a903a60f83
Vincent Obaro <vincentharyor@gmail.com>
2022-11-29 02:02:58 -0800

    prints + or - number

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,4 +5,5 @@ if number > 0:
     print(f"{number:d} is positive")
 elif number == 0:
     print(f"{number:d} is zero")
-else: print(f"{number:d} is negative")
+else:
+    print(f"{number:d} is negative")
