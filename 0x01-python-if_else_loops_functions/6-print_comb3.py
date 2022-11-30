d1c1627b847ad7dec9a7981db084d814977f72d8
Vincent Obaro <vincentharyor@gmail.com>
2022-11-30 02:57:59 -0800

    prints all combinations of different digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for i in range(10):
+    for j in range(10):
+        if i <j:
+            if i < 8:
+                print("{:d}{:d}, ".format(i,j), end="")
+            else:
+                print("{:d}{:d}".format(i,j))
