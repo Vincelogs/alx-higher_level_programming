ec9592b325833dace797f2812a244a73a00b7387
Vincent Obaro <vincentharyor@gmail.com>
2022-11-30 03:01:50 -0800

    fixed pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 
 for i in range(10):
     for j in range(10):
-        if i <j:
+        if i < j:
             if i < 8:
-                print("{:d}{:d}, ".format(i,j), end="")
+                print("{:d}{:d}, ".format(i, j), end="")
             else:
-                print("{:d}{:d}".format(i,j))
+                print("{:d}{:d}".format(i, j))
