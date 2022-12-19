b6cc4be7f342ee006e62f951960f9ade517ceb19
Vincent Obaro <vincentharyor@gmail.com>
2022-12-19 13:24:11 -0800

    divides element by element to list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,4 +16,4 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             result_list.append(result)
-    return result_list 
+    return result_list
