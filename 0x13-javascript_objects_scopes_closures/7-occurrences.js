76d5800836659db341366514abc7e4d5ebf4c490
vincelogs <vincentharyor@gmail.com>
2023-03-16 04:27:43 -0700

    Inherit from 'returns number of occurrence in a list

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let counter = 0;
+  for (let index = 0; index < list.length; index++) {
+    if (list[index] === searchElement) counter++;
+  }
+  return counter;
+};
