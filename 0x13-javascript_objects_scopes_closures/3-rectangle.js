87d9cc4a3262b46fe46094a53a08becfd3148a5b
vincelogs <vincentharyor@gmail.com>
2023-03-16 02:43:03 -0700

    manipulates rectangles functions

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.heigth = h;
+    }
+  }
+  
+  print () {
+    for (let i = 0; i < this.heigth; i++){
+      console.log('X'.repeat(this.width));
+    }
+  }
+}
