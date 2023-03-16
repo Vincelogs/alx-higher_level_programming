2d072518f68305e6a6b25b9818822ce941c7817d
vincelogs <vincentharyor@gmail.com>
2023-03-16 04:40:35 -0700

    Inherit from 'returns the reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // JS FUNCTION TO REVERSE A LIST
 exports.esrever = function (list) {
-  let newList = [];
+  const newList = [];
   for (let index = list.length - 1; index >= 0; index--) {
     newList.push(list[index]);
   }
