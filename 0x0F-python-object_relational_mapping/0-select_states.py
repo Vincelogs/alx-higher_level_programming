050fa6d3105dd8c64080f5a608c5d56c65ea2e33
vincelogs <vincentharyor@gmail.com>
2023-03-18 15:53:43 -0700

    Python script to select states from MySql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
     c.execute('SELECT * FROM states ORDER BY id ASC')
     [print(state) for state in c.fetchall()]
