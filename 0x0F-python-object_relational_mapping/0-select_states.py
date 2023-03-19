1d6ca873bfc1633c28731e7d4d476abe5223d0a1
vincelogs <vincentharyor@gmail.com>
2023-03-19 12:52:58 -0700

    script to select states from MySQL

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,3 +11,6 @@ if __name__ == "__main__":
     c = db.cursor()
     c.execute('SELECT * FROM states ORDER BY id ASC')
     [print(state) for state in c.fetchall()]
+
+    c.close()
+    db.close()
