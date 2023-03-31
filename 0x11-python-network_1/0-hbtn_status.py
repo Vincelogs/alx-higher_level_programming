1d5c9b87b5cd4c5226b9855bc38a52b83882f8d8
vincelogs <vincentharyor@gmail.com>
2023-03-31 07:39:03 -0700

    Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""  fetches https://alx-intranet.hbtn.io/status """
+
+if __name__ == "__main__":
+    from urllib.request import urlopen, Request
+
+    url = "https://alx-intranet.hbtn.io/status"
+    req = Request(url)
+    with urlopen(req) as res:
+        content = res.read()
+
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
